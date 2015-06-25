f00f0e083cfdaa256d4515739150f7c306d08a17
Karen Stolzenberg <kstolzenberg@gmail.com>
2015-06-25 19:12:51 -0400

    original name space

--- prk_story/tests/prk_test.py
+++ prk_story/tests/prk_test.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
 import requests
 import json
+from plotdevice import * 
 
-#gets all the data
+# plotdevice setup
+svg = ximport("svg")
+color(mode=RGB, range=255)
+
+# gets all the data from SF Open Data
 r = requests.get("https://data.sfgov.org/resource/uupn-yfaw.json") # these are offstreet from 2011
 p = requests.get("https://data.sfgov.org/resource/3gg2-z57m.json") # these are on-street from 2008 - onward
 
-#parse json into python objects
+# parse json into python objects
 data_off = json.loads(r.text)
 data_on = json.loads(p.text)
 
@@ -23,10 +29,11 @@ isolate(data_off, reg_stallct, 'regcap')
 isolate(data_off, valet_stallct, 'valetcap')
 isolate(data_on, street_sply,'prkng_sply')
 
+# add all the stalls and compare to size of SF
+# caveats: doesn't even include required parking for businesses or residences. ratio doesn't consider vertical space - multi story lots etc, but it's still helpful.
 total_stallct = sum(reg_stallct) + sum(valet_stallct) + sum(street_sply)
-total_prk_sf = total_stallct * (162) # 9'* 18' avg prk spot = 162 sq.ft // assumes all stalls the same size
-# SF Planning 154: min offstreet size = 8x18=144sf. ADA:9x18  Onstreet:20*8
-total_prk_area = total_prk_sf / 27878400 # 1sq mile is 27878400 sq ft
+total_prk_sf = total_stallct * (162) # 9'* 18' avg prk spot = 162 sq.ft // assumes all stalls the same size. SF Planning 154: min offstreet size = 8x18=144sf. ADA:9x18  Onstreet:20*8
+total_prk_area = total_prk_sf / 27878400 # 1sq mile is 27878400 sq ft // convert to square miles
 sf_area = 46.87 # sq miles per census bureau // you could subtract out remaining area of streets? not sure how that would overlap?
 prk_ratio = (total_prk_area / sf_area)
 
@@ -34,4 +41,12 @@ print "total stall ct: %g" % total_stallct
 print "total square miles of parking: %g" % total_prk_area
 print "ratio of parking to SF: %g \n rats thats not very high" % prk_ratio
 
-# doesn't even include required parking for businesses or residences
+# how do you know how to order things??
+
+# export graphic to local disk
+paths = svg.parse(open('/Users/karen/pyprojects/prk_story/tests/sf36.svg').read())
+for path in paths:
+    fill(255,255,255, 50)
+drawpath(paths)
+
+export("/Users/karen/pyprojects/prk_story/tests/trialSF.png")
