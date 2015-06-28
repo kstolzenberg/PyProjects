1a4d33dad2b0a6202af2f10cdff75743b4687b60
Karen <karen@Karens-MacBook-Air.local>
2015-06-28 19:32:39 -0400

    scaling and translation working

--- prk_story/tests/plotSF.py
+++ prk_story/tests/plotSF.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
+# enoding: utf-8
 import time
-
 start = time.time()
 
+import plotdevice
 
-from plotdevice import *
+print "time to import: %g" % (time.time()-start)
 
-svg = ximport("svg")
-color(mode=RGB, range=255)
-nofill()
+quit()
+
+svg = pd.ximport("svg")
+# pd.color(mode=RGB, range=255)
+pd.nofill()
 
 
 paths = svg.parse(open('/Users/karen/pyprojects/prk_story/tests/sf36.svg').read())
 
 # this still renders as solid black, when everything should be base-255?
 for path in paths:
-    drawpath(path)
+    pd.drawpath(path)
 
 # you need to center both of these around center! this documentation sucks
-size(500,500)
-export("/Users/karen/pyprojects/prk_story/tests/2.pdf")
-print "time to run: %g" % (time.time()-start)
+pd.size(500,500)
+pd.export("/Users/karen/pyprojects/prk_story/tests/2.pdf")
